39d59ddd4c448681672aaeab5efba64111e304ce
zuolina <zuolina1125@gmail.com>
2023-04-03 18:34:01 +0800

    Initial commit

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from common.parameterize_util import read_testcase_yaml
 
 if __name__ == '__main__':
     pytest.main()
-    time.sleep(3)
+    time.sleep(4)
     os.system('allure generate ./temps -o ./reports --clean')
     # print(read_testcase_yaml('/testcase/test_scrm1_1/test_login.yaml'))
 
